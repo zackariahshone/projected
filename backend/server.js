072afb6beaa79daad6c637ac3715042aa026e087
Zack Shone <zackariahshone.ta@gmail.com>
2022-02-13 19:53:35 -0600

    added routes correctly

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.get('/api', (req, res)=>{
 // Serve up static assets
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/build/index'));
+      });
   }
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build'));
-  });
