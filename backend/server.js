21b0bb076fbb87950d8790a705c8b05941e90475
Zack Shone <zackariahshone.ta@gmail.com>
2022-02-13 19:42:52 -0600

    fixing server

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,6 @@ app.get('/api', (req, res)=>{
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
   }
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build'));
   });
