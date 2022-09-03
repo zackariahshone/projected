98a2bc00af90ea2fb2bcc19309d4fb0d6e776ec1
Zack Shone <zackariahshone.ta@gmail.com>
2022-09-02 22:48:24 -0500

    filter works, need to figure out how to replace list

--- client/src/Containers/TruckSearch/index.js
+++ client/src/Containers/TruckSearch/index.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import {Container} from 'react-bootstrap'
+import React, { useEffect, useState } from 'react';
+import { Container } from 'react-bootstrap'
 import './style.css';
 // var ctx = document.getElementById("myChart1").getContext("2d");
 // console.log(ctx);
@@ -10,42 +10,47 @@ const testList = [
     "Foodology Food Truck",
     "Burntsugars Food Truck",
     "Fatt Fingers",
-    "Roll & Fold", 
+    "Roll & Fold",
     "Boondocks Grill",
     "City Pump",
     "Takashimura Hibachi - Rogers",
     "T-Mo's cajun Cookin"
-]  
+]
 
 
 const FoodTruckSearch = () => {
 
     const [truckList, setTruckList] = useState(testList);
 
-   const displaySearch = (searchTerm)=>{
-        console.log(searchTerm);
-   } 
+    const displaySearch = (searchTerm) => {
+        const result = truckList.filter(truck => truck.includes(searchTerm));
+    //    searchTerm !== null || searchTerm !== '' ? 
+       setTruckList(result);
+    //    setTruckList(testList)
+    }
+useEffect(()=>{
 
+})
     return (
         <Container>
-            <div id = 'searchInput'>
+            <div id='searchInput'>
                 <input
-                    onChange={(e)=>{
-                        displaySearch(e);
+                    onChange={(e) => {
+                        displaySearch(e.target.value);
                     }}
                 />
             </div>
-            <div id = 'searchResults'>
-            <ui>
+            <div id='searchResults'>
+                <ui>
 
-                {
-                    truckList.map((truck , i)=>(
-                        <li>
-                            {truck}
-                        </li>
-                    ))
-                }
-            </ui>
+                    {
+                        truckList.map((truck, i) => (
+                            <li>
+                                {truck}
+                            </li>
+                        ))
+                    }
+                </ui>
             </div>
         </Container>
     )
