ead2c872d9adc13634c3458e0936faf8848fbe3c
Zack Shone <zackariahshone.ta@gmail.com>
2022-12-05 18:31:16 -0600

    data is comming from the backend in a more real fasion, and FE is engesting data. added buttons for morespecific searches

--- backend/Routes/Data/truckRoutes.js
+++ backend/Routes/Data/truckRoutes.js
@@ -1,20 +1,85 @@
 const router = require('express').Router();
 
 router.get('/api/foodtrucklists', (req, res) => {
+  /**
+   * 
+   * dummy data to be pulled from DB as soon as available
+   * 
+   */
     const listofTrucks = {
       'listOfTrucks': [
-        "The Witching Hour",
-        "Food Therapy NWA",
-        "Foodology Food Truck",
-        "Burntsugars Food Truck",
-        "Fatt Fingers",
-        "Roll & Fold",
-        "Boondocks Grill",
-        "City Pump",
-        "Takashimura Hibachi - Rogers",
-        "T-Mo's cajun Cookin",
-        'new truck',
-        'one more'
+       {"name": "The Witching Hour",
+        "description":";lasdkjfl;sadjflsajdf",
+        "address":"1234 W Dr Rogers,AR",
+        "IMG":'www.com.comimage',
+        "dateAdded":"12/5/2022"  
+      },
+        {"name": "Food Therapy NWA",
+        "description":";lasdkjfl;sadjflsajdf",
+        "address":"1234 W Dr Rogers,AR",
+        "IMG":'www.com.comimage',
+        "dateAdded":"12/5/2022"  
+      },
+        {"name":"Foodology Food Truck",
+        "description":";lasdkjfl;sadjflsajdf",
+        "address":"1234 W Dr Rogers,AR",
+        "IMG":'www.com.comimage',
+        "dateAdded":"12/5/2022"  
+      },
+        {"name":"Burntsugars Food Truck",
+        "description":";lasdkjfl;sadjflsajdf",
+        "address":"1234 W Dr Rogers,AR",
+        "IMG":'www.com.comimage',
+        "dateAdded":"12/5/2022"  
+      },
+        {"name":"Fatt Fingers",
+        "description":";lasdkjfl;sadjflsajdf",
+        "address":"1234 W Dr Rogers,AR",
+        "IMG":'www.com.comimage',
+        "dateAdded":"12/5/2022"  
+      },
+        {"name":"Roll & Fold",
+        "description":";lasdkjfl;sadjflsajdf",
+        "address":"1234 W Dr Rogers,AR",
+        "IMG":'www.com.comimage',
+        "dateAdded":"12/5/2022"  
+      },
+        {"name":"Boondocks Grill",
+        "description":";lasdkjfl;sadjflsajdf",
+        "address":"1234 W Dr Rogers,AR",
+        "IMG":'www.com.comimage',
+        "dateAdded":"12/5/2022"  
+      },
+        {"name":"City Pump",
+        "description":";lasdkjfl;sadjflsajdf",
+        "address":"1234 W Dr Rogers,AR",
+        "IMG":'www.com.comimage',
+        "dateAdded":"12/5/2022"  
+      },
+        {"name":"Takashimura Hibachi - Rogers",
+        "description":";lasdkjfl;sadjflsajdf",
+        "address":"1234 W Dr Rogers,AR",
+        "IMG":'www.com.comimage',
+        "dateAdded":"12/5/2022"  
+      },
+        {"name":"T-Mo's cajun Cookin",
+        "description":";lasdkjfl;sadjflsajdf",
+        "address":"1234 W Dr Rogers,AR",
+        "IMG":'www.com.comimage',
+        "dateAdded":"12/5/2022"  
+      },
+        {"name":'new truck',
+        "description":";lasdkjfl;sadjflsajdf",
+        "address":"1234 W Dr Rogers,AR",
+        "IMG":'www.com.comimage',
+        "dateAdded":"12/5/2022"  
+      },
+        {"name":'one more',
+        "description":";lasdkjfl;sadjflsajdf",
+        "address":"1234 W Dr Rogers,AR",
+        "IMG":'www.com.comimage',
+        "dateAdded":"12/5/2022"  
+      }
       ]
     }
     res.send(listofTrucks)
--- client/src/Containers/TruckSearch/trucksearch.js
+++ client/src/Containers/TruckSearch/trucksearch.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { Container } from 'react-bootstrap'
+import TruckSearchFilterButtons from './truckSearchFilterButtons';
+import { Container, Row, Col } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
-import {truckSearchList,loadReducer} from '../../appstore/Reducers/TruckSearch'
-import {getData} from '../../genUtils/requests'
+import { truckSearchList, loadReducer } from '../../appstore/Reducers/TruckSearch';
+import { getData } from '../../genUtils/requests';
 import './style.css';
 // var ctx = document.getElementById("myChart1").getContext("2d");
 // console.log(ctx);
@@ -11,34 +12,53 @@ const FoodTruckSearch = () => {
     const [truckList, setTruckList] = useState(foodTruckList);
     const [searchTerm, setSearchTerm] = useState('');
     // dispatch(loadReducer({test:'string'}))
-    useEffect(()=>{
-            getData('api/foodtrucklists','GET',{},loadReducer,{});
-    },[truckList, foodTruckList]);
-    useEffect(()=>{
-    const result = truckList?.filter(truck => truck.toLowerCase().includes(searchTerm.toLowerCase()));
-    searchTerm !== '' ? setTruckList(result): setTruckList(foodTruckList);
-},[searchTerm])
+    useEffect(() => {
+        getData('api/foodtrucklists', 'GET', {}, loadReducer, {});
+    }, [truckList, foodTruckList]);
+    useEffect(() => {
+        const result = truckList?.filter(truck => truck.name.toLowerCase().includes(searchTerm.toLowerCase()));
+        searchTerm !== '' ? setTruckList(result) : setTruckList(foodTruckList);
+    }, [searchTerm])
 
     return (
         <Container>
-            <div id='searchInput'>
-                <input
-                    onChange={(e) => {
-                        setSearchTerm(e.target.value)
-                    }}
-                />
-            </div>
+
+            <input
+                className='searchInput'
+                onChange={(e) => {
+                    setSearchTerm(e.target.value)
+                }}
+            />
+            {/* <div className='filterButton'> */}
+            <TruckSearchFilterButtons />
+            {/* </div> */}
             <div id='searchResults'>
-                <ul>
+                <Row>
 
                     {
-                        truckList?.map((truck, i) => (
-                            <li key = {`${i}_truckname`}>
-                                {truck}
-                            </li>
+                        truckList?.map(({ name, description, address, dateAdded }, i) => (
+                            <Col xs={12} md={6} lg={4} xl={4} >
+
+                                <div className='truckCard'>
+                            <Row>
+                            <Col>
+
+                                    <li key={`${i}_truckname`}>
+                                        {name}
+                                    </li>
+                                    <p> <b>about the truck:</b> {description}</p>
+                                    <p> <b>here we are:</b> {address}</p>
+                                    <p> <b>been on app since:</b> {dateAdded}</p>
+                            </Col>
+                            <Col>
+                                <div className='mockImg'></div>
+                            </Col>
+                            </Row>
+                                </div>
+                            </Col>
                         ))
                     }
-                </ul>
+                </Row>
             </div>
         </Container>
     )
