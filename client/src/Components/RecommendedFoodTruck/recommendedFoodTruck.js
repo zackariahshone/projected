bef8fbbbfd2c3cbd83272a81be9084638bc1b335
Zack Shone <zackariahshone.ta@gmail.com>
2022-12-09 23:44:46 -0600

    selectables in place and need style

--- client/src/Components/RecommendedFoodTruck/recommendedFoodTruck.js
+++ client/src/Components/RecommendedFoodTruck/recommendedFoodTruck.js
@@ -1,28 +1,24 @@
 // import { data } from 'jquery';
-import { Button, Container, Form } from 'react-bootstrap';
-import { getData } from '../../genUtils/requests'
+import { Container } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import { 
+    truckCategories
+    } from '../../appstore/Reducers/TruckSearch';
+
 const RecommendedTrucks = () => {
+
+    const categories = useSelector(truckCategories);
+
+   Object.values(categories).forEach(e=>{
+    console.log(e)
+   })
     return (
         // <></>
         <Container>
-            <Form>
-                <Form.Group>
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
-                <Button
-                    title={"Push this button"}
-                    onClick={() => {
-                      getData().then((response) => {
-                        })
-                    }}
-                >
-                    Push me Please!!!
-                </Button>
-            </Form>
+            <h1>Find Your Flavor! </h1>
+            { Object.values(categories).map(e=>(
+                <button>{e}</button>
+   ))}
         </Container>
     );
 }
