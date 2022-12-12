e2b6fd1fb8f4689777e995a44a805f103246dbd8
Zack Shone <zackariahshone.ta@gmail.com>
2022-12-12 09:13:02 -0600

    restore npm packages on client side.

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const routes = require('./Routes');
 const cors = require("cors");
 const bodyParser = require('body-parser')
-require('./dbconnection/connection');
+// require('./dbconnection/connection');
 
 // Initialize the app and create a port
 const app = express();
@@ -22,3 +22,15 @@ app.listen(PORT, () => console.log(`Listing on  port ${PORT}`));
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
+// app.use((req, res, next) => {
+//   const allowedOrigins = ['http://127.0.0.1:3000', 'http://localhost:3000', 'http://127.0.0.1:5000', 'http://localhost:5000'];
+//   const origin = req.headers.origin;
+//   if (allowedOrigins.includes(origin)) {
+//        res.setHeader('Access-Control-Allow-Origin', origin);
+//   }
+//   //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
+//   res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.header('Access-Control-Allow-Credentials', true);
+//   return next();
+// });
