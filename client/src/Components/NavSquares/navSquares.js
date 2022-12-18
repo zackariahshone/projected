41c44c7ec2b14d11061c8159264c10c78959ff0d
Zack Shone <zackariahshone.ta@gmail.com>
2022-12-18 14:07:11 -0600

    fixed validation to check user/protected pages

--- client/src/Components/NavSquares/navSquares.js
+++ client/src/Components/NavSquares/navSquares.js
@@ -1,44 +1,51 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
 import {
     Col,
     Row,
     Container,
     Nav
-} from 'react-bootstrap'
+} from 'react-bootstrap';
 import './style.css';
 import { colorArray, ROUTES } from '../../GlobalConstanst';
+import { isLoggedIn } from '../../appstore/Reducers/UserReducers';
 
 const NavSquares = () => {
     let colorIndex = 0;
     let textIndex = colorArray.length - 1;
+    const loggedInStatus = useSelector(isLoggedIn);
+
     return (
-        <Container 
-            style = {{marginTop: '10%'}}
+        <Container
+            style={{ marginTop: '10%' }}
         >
             <Row>
-                {ROUTES.map((route,x) => {
-
-                if(x > 0){
-                colorIndex++;
-                textIndex--;
-                }
-                colorIndex = colorArray.length === colorIndex ? 0: colorIndex;
-                textIndex = textIndex === -1 ?  colorArray.length-1 : textIndex ;   
-                  return(  <Col key = {`col_${x}`} xs={12} md={6} lg={4} xl={4}>
-                        <div key = {`square_${x}`} 
-                             className={`navSquare ${route.link}`}
-                             style={{backgroundColor:`${colorArray[colorIndex]}`}}
-                             >
-                            <Nav.Link key = {`square_link_${x}`} 
-                                      href={route.link}
-                                      style={{textDecoration:'none','color':`${colorArray[textIndex]}`}}
-                                      >
-                                <div key = {`square_info_${x}`} className= "squareText">
+                {ROUTES.map((route, x) => {
+                    if (route.protected && !loggedInStatus) {
+                        return;
+                    }
+                    if (x > 0) {
+                        colorIndex++;
+                        textIndex--;
+                    }
+                    colorIndex = colorArray.length === colorIndex ? 0 : colorIndex;
+                    textIndex = textIndex === -1 ? colorArray.length - 1 : textIndex;
+                    return (<Col key={`col_${x}`} xs={12} md={6} lg={3} xl={3}>
+                        <div key={`square_${x}`}
+                            className={`navSquare ${route.link}`}
+                            style={{ backgroundColor: `${colorArray[colorIndex]}` }}
+                        >
+                            <Nav.Link key={`square_link_${x}`}
+                                href={route.link}
+                                style={{ textDecoration: 'none', 'color': `${colorArray[textIndex]}` }}
+                            >
+                                <div key={`square_info_${x}`} className="squareText">
                                     {route.name}
                                 </div>
                             </Nav.Link>
                         </div>
                     </Col>
-                  )
+                    )
                 })}
             </Row>
         </Container>
--- client/src/Containers/TopNav/topNav.js
+++ client/src/Containers/TopNav/topNav.js
@@ -1,6 +1,6 @@
 import { Fragment, useState } from 'react';
-import { useSelector,useDispatch } from 'react-redux';
-import {  logout, isLoggedIn,currentUser } from '../../appstore/Reducers/UserReducers';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout, isLoggedIn, currentUser } from '../../appstore/Reducers/UserReducers';
 import { ROUTES } from '../../GlobalConstanst';
 import {
   Navbar,
@@ -24,25 +24,25 @@ const TopNav = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              {ROUTES.map((nav,x) => {
+              {ROUTES.map((nav, x) => {
                 /**
                  * check status of user and route in order to expose
                  */
-                if(nav.protected && !loggedInStatus){
+                if (nav.protected && !loggedInStatus) {
                   return;
                 }
-               return <Nav.Link key = {`nav_${x}`} href={nav.link}>{nav.name}</Nav.Link>
+                return <Nav.Link key={`nav_${x}`} href={nav.link}>{nav.name}</Nav.Link>
               })}
             </Nav>
             <Nav className="justify-content-end">
               <Nav.Link
-                onClick={()=>{
-                  if(loggedInStatus){
-                     dispatch(logout({ value: false, type: 'logout' }));
+                onClick={() => {
+                  if (loggedInStatus) {
+                    dispatch(logout({ value: false, type: 'logout' }));
                   }
                 }}
-                 href="/login"> {loggedInStatus ? `Hi ${userInfo.firstName}! Sign Out Here` : `Sign In`} 
-                 
+                href="/login"> {loggedInStatus ? `Hi ${userInfo.firstName}! Sign Out Here` : `Sign In`}
+
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
