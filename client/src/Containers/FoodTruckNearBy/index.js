d7f845d217db2d9ef6dc63060afe647ab3f3f0e3
Zack Shone <zackariahshone.ta@gmail.com>
2022-12-24 21:06:19 -0500

    correct import

--- client/src/Containers/FoodTruckNearBy/index.js
+++ client/src/Containers/FoodTruckNearBy/index.js
@@ -3,9 +3,10 @@ import { Container, Row, Col } from 'react-bootstrap'
 import { useSelector } from 'react-redux';
 import { truckSearchList } from '../../appstore/Reducers/TruckSearch';
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
-import {GOOGLE_MAPS_API} from "../../hiddenvariable";
 import './style.css'
-
+if(process.env.NODE_ENV !== production){   
+    const GOOGLE_MAPS_API =  require('../../hiddenvariable').GOOGLE_MAPS_API;
+} 
 const googleKey = GOOGLE_MAPS_API ? GOOGLE_MAPS_API:process.env.GOOGLE_MAPS_API;
 console.log(googleKey)
 export function CustomMap({ google }) {
