61ba723340f7dc9f0f379b7c35bb396981141f6d
Zack Shone <zackariahshone.ta@gmail.com>
2022-12-26 22:09:02 -0500

    better load of data

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom'
 import { BROWSER_ROUTER_CONFIGS } from './GlobalConstanst';
 import TopNav from './Containers/TopNav/topNav';
+import { loadReducer,setCategories } from './appstore/Reducers/TruckSearch';
+import { getData } from './genUtils/requests';
+
 import './App.css';
 
 
 
 
 function App() {
+  useEffect(() => {
+    getData('api/foodtrucklists', 'GET', {}, loadReducer, {});
+    getData('api/getcategories','GET',{},setCategories,{});
+}, []);
   return (
     <Fragment>
       <TopNav />
