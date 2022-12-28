335faf3ca83c6a0c8ef1be3be448d54aa52803ca
Zack Shone <zackariahshone.ta@gmail.com>
2022-12-27 22:41:14 -0500

    dsa;lfkj

--- client/src/Containers/NewTrucks/newTrucks.js
+++ client/src/Containers/NewTrucks/newTrucks.js
@@ -1,8 +1,8 @@
-import React,{Fragment} from 'react';
+import React, { Fragment } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { truckSearchList } from '../../appstore/Reducers/TruckSearch';
-import {sortByDate} from '../../genUtils'
+import { sortByDate } from '../../genUtils'
 import './style.css'
 // import { Doughnut } from 'react-chartjs-2';
 
@@ -10,53 +10,53 @@ import './style.css'
 // var ctx = document.getElementById("myChart1").getContext("2d");
 const NewTrucks = () => {
   const foodTruckList = useSelector(truckSearchList)
-  
+
   const newFoodTrucks = []
-  foodTruckList.forEach((truck,i)=>{
-    if(i<5){
+  foodTruckList.forEach((truck, i) => {
+    if (i < 5) {
       newFoodTrucks.push(truck);
     }
   })
-  console.log(5%3);
+  console.log(5 % 3);
   // console.log(sortByDate(foodTruckList));
-  
 
-    return (
-       <Container>
-       <Container>
-          <h1>Recently Added Food Trucks!</h1>
-       </Container>
-        <Row>
-        {newFoodTrucks.map((truck,x)=>{
-          return(
+
+  return (
+    <Container>
+      <Container>
+        <h1>Recently Added Food Trucks!</h1>
+      </Container>
+      <Row>
+        {newFoodTrucks.map((truck, x) => {
+          return (
             <Fragment>
 
-            <Col xs={6}  md={x>2?{span: 1, offset: 1}:2} lg={2}>
-              <img 
-                src={truck.IMG} 
-                alt={'truckImg'}
+              <Col xs={6} md={x > 2 ? { span: 1, offset: 1 } : 2} lg={2}>
+                <img
+                  src={truck.IMG}
+                  alt={'truckImg'}
                 />
-            </Col>
-            <Col
-             xs={6}  md={2} lg={2}
-            >
-            <div
-              class = 'truckDetails'
-            >
-              <ul>
-                  <li>{truck.name}</li>
-                  <li>{truck.address}</li>
-                  <li>{truck.description}</li>
-              </ul>
-            </div>
-            </Col>
+              </Col>
+              <Col
+                xs={6} md={2} lg={2}
+              >
+                <div
+                  class='truckDetails'
+                >
+                  <ul>
+                    <li>{truck.name}</li>
+                    <li>{truck.address}</li>
+                    <li>{truck.description}</li>
+                  </ul>
+                </div>
+              </Col>
             </Fragment>
           )
         })}
 
-        </Row>
-       </Container>
-    )
+      </Row>
+    </Container>
+  )
 }
 
 export default NewTrucks;
