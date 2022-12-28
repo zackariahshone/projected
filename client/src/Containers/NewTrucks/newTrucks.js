f9050af94b4f48b3dc9583f8db99fc3d5e06130b
Zack Shone <zackariahshone.ta@gmail.com>
2022-12-27 19:34:24 -0500

    make new trucks better next is logic

--- client/src/Containers/NewTrucks/newTrucks.js
+++ client/src/Containers/NewTrucks/newTrucks.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React,{Fragment} from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { truckSearchList } from '../../appstore/Reducers/TruckSearch';
+import {sortByDate} from '../../genUtils'
 import './style.css'
 // import { Doughnut } from 'react-chartjs-2';
 
@@ -9,19 +10,34 @@ import './style.css'
 // var ctx = document.getElementById("myChart1").getContext("2d");
 const NewTrucks = () => {
   const foodTruckList = useSelector(truckSearchList)
-
+  console.log(foodTruckList);
     return (
        <Container>
         <Row>
         {foodTruckList.map((truck)=>{
           return(
+            <Fragment>
 
-            <Col xs={12} md={4} lg={3}>
-              <p>{truck.name}</p>
+            <Col xs={6} md={2} lg={2}>
               <img 
                 src={truck.IMG} 
-                alt={'truckImg'}/>
+                alt={'truckImg'}
+                />
             </Col>
+            <Col
+             xs={6} md={2} lg={2}
+            >
+            <div
+              class = 'truckDetails'
+            >
+              <ul>
+                  <li>{truck.name}</li>
+                  <li>{truck.address}</li>
+                  <li>{truck.description}</li>
+              </ul>
+            </div>
+            </Col>
+            </Fragment>
           )
         })}
 
--- client/src/genUtils/index.js
+++ client/src/genUtils/index.js
@@ -2,3 +2,12 @@ export const dateFormat = (date) =>{
 const formatedDate = new Date(date);
 return formatedDate.toLocaleDateString();
 }
+
+export const sortByDate =(array)=>{
+   array.sort(function(a,b){
+        // Turn your strings into dates, and then subtract them
+        // to get a value that is either negative, positive, or zero.
+        return new Date(b.dateAdded) - new Date(a.dateAdded);
+      });
+      return array;
+} 
