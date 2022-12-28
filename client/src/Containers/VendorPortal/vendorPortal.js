78a1c93d33b1211009950fd0adde346d5df58cc7
Zack Shone <zackariahshone.ta@gmail.com>
2022-12-28 18:08:26 -0500

    sign in to vendor portal

--- client/src/Containers/VendorPortal/vendorPortal.js
+++ client/src/Containers/VendorPortal/vendorPortal.js
@@ -1,6 +1,5 @@
 import React, { Fragment, useState } from "react";
-import { Container, Col, Row } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Container, Col, Row,Form,Button } from "react-bootstrap";
 import { DUMMY_IMG } from "../../GlobalConstanst";
 import { CreateTruck } from "./CreateTruck";
 import { EditTruck } from "./EditTruck";
@@ -8,14 +7,19 @@ import './style.css'
 
 export const VendorPortal = () => {
     const [selection, setSelection] = useState();
+    const [userCred, setUserCred]=useState();
+    const [signedin, setSignedIn] = useState(false);
+    const handleClick = ()=>{
+        setSignedIn(true);
+    }
     const directOptions = (selection) => {
         switch (selection) {
             case 'createTruck':
-                return <CreateTruck/>;
+                return <CreateTruck />;
             case 'editTruck':
-                return <EditTruck/>;
+                return <EditTruck />;
             default:
-                return <h2>Make a Selection</h2>;
+                return <h2>Sign in please and then make a selection</h2>;
         }
     }
     return (
@@ -31,24 +35,85 @@ export const VendorPortal = () => {
                     </div>
                 </Col>
                 <Col>
-                    <div className="vendorInputForm">
-                        <ui>
-                            <li>
-                                <button
-                                    onClick={(e) => {
-                                        setSelection("createTruck")
+                    {!signedin ?
+                        <Col>
+                            <Form>
+                                {/* {emailError ? <p className='error'> * Not an email</p> : null} */}
+                                <Form.Group className="mb-3" controlId="formBasicEmail">
+                                    <Form.Label>Email address</Form.Label>
+                                    <Form.Control
+                                        type="email"
+                                        placeholder="Enter email"
+                                        onChange={(e) => {
+                                            setUserCred({
+                                                ...userCred,
+                                                email: e.target.value
+                                            })
+                                            // setEmailError(false);
+                                        }}
+                                        // onBlur={(e) => {
+
+                                        //     if (!String(e.target.value)
+                                        //         .toLowerCase()
+                                        //         .match(
+                                        //             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                                        //         )) {
+                                        //         setEmailError(true)
+                                        //     }
+                                        // }}
+                                    />
+                                </Form.Group>
+                                {/* {pwdError ? <p className='error'>*password cannot be blank</p> : null} */}
+                                <Form.Group className="mb-3" controlId="formBasicPassword">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control
+                                        type="password"
+                                        placeholder="Password"
+                                        onChange={(e) => {
+                                            setUserCred(
+                                                {
+                                                    ...userCred,
+                                                    password: e.target.value
+                                                })
+                                            // setPwdError(false);
+                                        }}
+                                        // onBlur={(e) => {
+                                        //     if (e.target.value === '') {
+                                        //         setPwdError(true)
+                                        //     }
+                                        // }}
+                                    />
+                                </Form.Group>
+                                <Button
+                                    variant="primary"
+                                    onClick={() => {
+                                        handleClick();
                                     }}
-                                >Create Truck</button>
-                            </li>
-                            <li>
-                                <button
-                                    onClick={(e) => {
-                                        setSelection("editTruck")
-                                    }}
-                                >Edit Trucks</button>
-                            </li>
-                        </ui>
-                    </div>
+                                >
+                                    Submit
+                                </Button>
+                            </Form>
+
+                        </Col>
+                        :
+                        <div className="vendorInputForm">
+                            <ui>
+                                <li>
+                                    <button
+                                        onClick={(e) => {
+                                            setSelection("createTruck")
+                                        }}
+                                    >Create Truck</button>
+                                </li>
+                                <li>
+                                    <button
+                                        onClick={(e) => {
+                                            setSelection("editTruck")
+                                        }}
+                                    >Edit Trucks</button>
+                                </li>
+                            </ui>
+                        </div>}
                 </Col>
             </Row>
             <Row>
