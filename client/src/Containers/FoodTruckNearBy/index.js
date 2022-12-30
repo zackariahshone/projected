d86ff081efe42f518f633a7d89123c5ce49817cd
Zack Shone <zackariahshone.ta@gmail.com>
2022-12-30 10:48:16 -0500

    geo location added

--- client/src/Containers/FoodTruckNearBy/index.js
+++ client/src/Containers/FoodTruckNearBy/index.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { useEffect,useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap'
 import { useSelector } from 'react-redux';
 import { truckSearchList } from '../../appstore/Reducers/TruckSearch';
 import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
 import './style.css'
 // const ckey = require('ckey')
+const GeoLoc = navigator.geolocation;
 const googleKey = process.env.REACT_APP_GOOGLE_MAPS_API;
 export function CustomMap({ google }) {
-    const markerSet = [];
+    const [userLoc, setUserLoc] = useState();
     const foodTruckList = useSelector(truckSearchList)
-    foodTruckList.forEach((truck) => {
-        if(truck.coordinates){
-
-            const { lat, lon } = truck?.coordinates;
-            markerSet.push({ lat: lat, lng: lon })
-        }
+    GeoLoc.getCurrentPosition((loc)=>{
+        setUserLoc(
+            {
+                lat:loc.coords?.latitude,
+                lng:loc.coords?.longitude
+            }
+        )
     })
+    // const getMapConfig =()=>{
+        const markerSet = [];
+        const nameSet = [];
+        foodTruckList.forEach((truck) => {
+            if(truck.coordinates){
+                const { lat, lon } = truck?.coordinates;
+                markerSet.push({ lat: lat, lng: lon })
+                nameSet.push({name:truck.name})
+            }
+        })
+        // return {nameSet,markerSet}
+    // }
     return (
         <Container>
             <Row>
@@ -42,9 +56,13 @@ export function CustomMap({ google }) {
                         disableDefaultUI={true}
                     >
                         {markerSet.map(
-                            coords => <Marker position={coords} title={'test title'} />
+                            (coords, i) => <Marker position={coords} title={`${nameSet[i].name}`} />
                         )}
-
+                        {/* lat:GeoLoc.coords?.latitude,
+                    lng:GeoLoc.coords?.longitude */
+                            console.log(userLoc)
+                    }
+                        <Marker position={userLoc} title={'you are here'}/>
                     </Map>
                 </Col>
             </Row>
