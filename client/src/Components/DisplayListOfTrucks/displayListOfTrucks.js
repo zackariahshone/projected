2d933d7ae0dc57f826e313e647673b13fa7c1ff5
Zack Shone <zackariahshone.ta@gmail.com>
2023-10-07 19:43:01 -0500

    added style to side bar on map for mobile

--- client/src/Components/DisplayListOfTrucks/displayListOfTrucks.js
+++ client/src/Components/DisplayListOfTrucks/displayListOfTrucks.js
@@ -1,39 +1,44 @@
-import React, { Fragment } from "react";
+import React, { Fragment,useState } from "react";
 import { Row, Col, Container } from "react-bootstrap";
 import "./style.css";
-
+import {HoverDetailsComponent} from "../HoverDetails";
 
 export const TruckListDisplay = ({ trucks }) => {
+    const [selectedTruck, setSelectedTruck] = useState(false);
     return (
         // <Container>
+        <>
+            {trucks ? trucks.map((truck) => (
                 <>
+                    <Row className='singleTruck' 
+                            onClick ={(e)=>{
+                            
+                                selectedTruck === true ? setSelectedTruck(false) : setSelectedTruck(true);
+                         }} 
+                    >
 
-                {trucks ? trucks.map((truck) => (
-                    <>
-            <Row className='singleTruck' >
+                        <Col xs={12} sm={12} md={4}>
+                            <img className="singleTruckImg" src={truck.IMG} alt={'truckimage'} />
+                        </Col>
+                        <Col 
+                            className={'displayTruckDetails scroll'}
+                         >
 
-                    <Col
-                          xs={12}  sm ={12} md ={4}
-                        >
-                                <img className="singleTruckImg" src={truck.IMG} 
-                                alt={'truck image'}
-                                />
-                    </Col>
-                    <Col className ={'displayTruckDetails scroll'} >
-                        <b>{truck.name}</b>
-                        <Row>
-                            <Col xs={12}>open</Col>
-                            <Col>{truck?.address}</Col>
-                        </Row>
-                        <Row>
-                            <Col>Reviews: 4.5/5</Col>
-                        </Row>
-                    </Col>
-                    < br />
-            </Row>
-                    </>
-                )) : 'trucks empty'}
+                            <HoverDetailsComponent clicked ={selectedTruck}  truckData = {truck}/>
+                            <b>{truck.name}</b>
+                            <Row>
+                                <Col xs={12}>open</Col>
+                                <Col>{truck?.address}</Col>
+                            </Row>
+                            <Row>
+                                <Col>Reviews: 4.5/5</Col>
+                            </Row>
+                        </Col>
+                        < br />
+                    </Row>
                 </>
+            )) : 'trucks empty'}
+        </>
         // </Container>
     )
 }
